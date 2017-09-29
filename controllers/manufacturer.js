a346d786b20ef10932af6768ef09c23bf4b1e816
Neciu Dan <cst2989@gmail.com>
2017-09-29 15:37:19 +0300

    Added create manufacturer

--- controllers/manufacturer.js
+++ controllers/manufacturer.js
@@ -7,7 +7,21 @@ const manufacturerController = {
       // Returns all manufacturers
         Manufacturer.find({})
             .exec((err, manufacturers) => res.json(manufacturers))
-    }
+    },
+    create (req, res) {
+        const requestBody = req.body;
+        // Creates a new record from a submitted form
+        const newManufacturer = new Manufacturer(requestBody);
+        // and saves the record to
+        // the data base
+        newManufacturer.save( (err, saved) => {
+            // Returns the saved product
+            // after a successful save
+            Manufacturer
+                .findOne({_id: saved._id})
+                .exec((err, manufacturer) => res.json(manufacturer));
+        } )
+    },
 };
 
 module.exports = manufacturerController;
--- routes/api/index.js
+++ routes/api/index.js
@@ -5,7 +5,7 @@ const productController      = require('../../controllers/product')
 const manufacturerController = require('../../controllers/manufacturer')
 
 router.get('/manufacturers', manufacturerController.all);
-
+router.post('/manufacturers', manufacturerController.create);
 router.get('/products', productController.all);
 router.get('/products/:id', productController.byId);
 router.post('/products', productController.create);
